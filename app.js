d09f2c4ef005b54c9ab867eef18c6ebf57e59775
Elisha Malinski <elishamalinski@gmail.com>
2024-02-23 11:22:50 -0600

    Completed assignment

---
+++ app.js
@@ -0,0 +1,55 @@
+// Same keys and values
+// function createInstructor(firstName, lastName){
+//   return {
+//     firstName: firstName,
+//     lastName: lastName
+//   }
+// }
+
+// Same keys and values ES2015
+/* Write an ES2015 Version */
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName,
+    };
+  };
+// Computed Property Names
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+// Computed Property Names ES2015
+/* Write an ES2015 Version */
+let favoriteNumber = 42;
+
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+}
+
+
+// Object Methods
+// var instructor = {
+//   firstName: "Colt",
+//   sayHi: function(){
+//     return "Hi!";
+//   },
+//   sayBye: function(){
+//     return this.firstName + " says bye!";
+//   }
+// }
+
+const instructor2 ={
+    firstName: "Colt",
+    sayHi(){
+        return "Hi!";
+    },
+    sayBye(){
+        return "Bye!";
+    },
+}
